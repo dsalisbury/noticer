c34192a143d84f526cdab317875d4f7ea12874ec
David Salisbury <david@dsho.me>
2015-12-14 20:52:47 +0000

    tests: stop repeating queueing of stuff

--- test_noticer.py
+++ test_noticer.py
@@ -5,29 +5,26 @@ from unittest.mock import patch
 from noticer import runner, STOP, RELOAD
 
 
+def enqueue(*things):
+    tasks = queue.PriorityQueue()
+    for i, thing in enumerate(things):
+        tasks.put((i, thing))
+    return tasks
+
+
 @patch('subprocess.Popen', autospec=True)
 class RunnerTests(unittest.TestCase):
     def test_execution(self, popen):
         test_cmd = 'run_my_thing.sh'
-        tasks = queue.PriorityQueue()
-        tasks.put((0, STOP))
-        runner(tasks, test_cmd)
+        runner(enqueue(STOP), test_cmd)
         popen.assert_called_with(test_cmd)
 
     def test_reload(self, popen):
-        tasks = queue.PriorityQueue()
-        tasks.put((0, RELOAD))
-        tasks.put((0, RELOAD))
-        tasks.put((0, RELOAD))
-        tasks.put((1, STOP))  # lower priority than the RELOADs
-        runner(tasks, 'gogo')
+        runner(enqueue(RELOAD, RELOAD, RELOAD, STOP), 'gogo')
         self.assertEqual(popen.call_count, 4)
 
     def test_short_running_doesnt_get_signal(self, popen):
         proc = popen.return_value
         proc.poll.return_value = True
-        tasks = queue.PriorityQueue()
-        tasks.put((0, RELOAD))
-        tasks.put((1, STOP))
-        runner(tasks, 'gogo')
+        runner(enqueue(RELOAD, STOP), 'gogo')
         popen.return_value.send_signal.assert_not_called()
