f43fd1f0361edc8d0a0ec4064033a81d2106bfab
David Salisbury <david@dsho.me>
2015-12-14 21:10:30 +0000

    runner: tests around handling of long-running processes

--- test_noticer.py
+++ test_noticer.py
@@ -1,4 +1,6 @@
 import queue
+import signal
+import subprocess
 import unittest
 from unittest.mock import patch
 
@@ -28,3 +30,19 @@ class RunnerTests(unittest.TestCase):
         proc.poll.return_value = True
         runner(enqueue(RELOAD, STOP), 'gogo')
         popen.return_value.send_signal.assert_not_called()
+
+    def test_long_running_but_quick_stopping(self, popen):
+        proc = popen.return_value
+        proc.poll.return_value = False
+        proc.wait.return_value = True
+        runner(enqueue(STOP), 'gogo')
+        proc.send_signal.assert_called_with(signal.SIGINT)
+        self.assertEqual(popen.return_value.send_signal.call_count, 1)
+
+    def test_long_running_slow_stopping(self, popen):
+        proc = popen.return_value
+        proc.poll.return_value = False
+        proc.wait.side_effect = [
+            subprocess.TimeoutExpired('gogo', 2), lambda *_: True]
+        runner(enqueue(STOP), 'gogo')
+        self.assertTrue(proc.kill.called)
